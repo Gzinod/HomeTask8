ea580c8a6e98e67a26813a1465021dc978f0a2e7
GzINOD <gzinod31@rambler.ru>
2022-11-14 21:49:30 +0300

    Homewrok is done

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("input number1");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = 2;
+
+while(number2<=number1)
+{
+if(number2%2==0)
+{
+    Console.WriteLine(number2);
+    number2=number2+1;
+}
+else number2=number2+1;
+}
