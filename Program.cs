7de93f7d509265695065c4fc5aed0cd918c24011
GzINOD <gzinod31@rambler.ru>
2022-11-15 19:19:42 +0300

    homework again

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿Console.WriteLine("input number1");
 int number1 = Convert.ToInt32(Console.ReadLine());
-int number2 = 2;
+int number2 = 1;
+if(number1>=0)
+{
+
 
 while(number2<=number1)
 {
@@ -10,4 +13,16 @@ if(number2%2==0)
     number2=number2+1;
 }
 else number2=number2+1;
+}
+}
+else 
+
+while(number2>=number1)
+{
+if(number2%2==0)
+{
+    Console.WriteLine(number2);
+    number2=number2-1;
+}
+else number2=number2-1;
 }
